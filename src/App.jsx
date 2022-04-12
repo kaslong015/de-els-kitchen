b824b16814f2ec8e109c82a2a7577906753b54a5
kaslong <kevinkaslong@gmail.com>
2022-04-12 22:38:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+
+
+  return (
+    <div className="App">
+      <h1 className="text-5xl text-red-400 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{jsx,ts,tsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
