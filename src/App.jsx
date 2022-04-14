cdc3f7c3e3f7951456abe23b78985124df038a98
kaslong <kevinkaslong@gmail.com>
2022-04-14 17:49:47 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
-
-
   return (
     <div className="App">
-      <h1 className="text-5xl text-red-400 font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='bg-gray-900 text-white py-4 fixed'>
+      <div className='container flex justify-between mx-auto items-center' >
+        <div className='logo'>logo</div>
+        <div className='flex flex-row gap-4 text-base font-base items-center'>
+          <ul className='flex flex-row gap-12 md:px-64'>
+            <li className='hover:text-yellow-300 transition ease-in-out duration-500'><a href="">Home</a></li>
+            <li className='hover:text-yellow-300 transition ease-in-out duration-500'><a href="">Home</a></li>
+            <li className='hover:text-yellow-300 transition ease-in-out duration-500'><a href="">Home</a></li>
+          </ul>
+          <button className='hover:bg-yellow-900 transition ease-in-out duration-500 border-yellow-700 border-2 px-4 py-1 rounded-full'>login</button>
+        </div>
+      </div>
+    </header>
+
   )
 }
 
