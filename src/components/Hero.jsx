a5be15cf21d21831f696e23b315c347b6fd404a2
kaslong <kevinkaslong@gmail.com>
2022-04-22 00:51:10 +0100

    full responsive at menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
                 <div className="flex flex-col text-white text-4xl text-center w-full justify-center items-center">
                     <p className="text-[#fd9d3e] font-bold md:pt-56 pt-32">RESTAURANT / CAFE</p>
                     <h1 className="text-left text-4xl md:text-8xl md:text-center font-extrabold ">Quality foods</h1>
-                    <a href="" className="mt-8 px-4 py-2 text-base border-[#fd9d3e] border rounded-full bg-[#82b440] uppercase  hover:bg-transparent hover:border-[#fd9d3e] transition ease-in-out duration-300">place your order</a>
+                    <a href="" className="mt-8 mb-28 px-4 py-2 text-base border-[#fd9d3e] border rounded-full bg-[#82b440] uppercase  hover:bg-transparent hover:border-[#fd9d3e] transition ease-in-out duration-300">place your order</a>
                 </div>
             </div>
         </section>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,9 +5,25 @@ import order3 from '../order-3.svg';
 
 const Menu = () => {
     return (
-        <section className="bg-[#fd9d3e] h-96">
-            <div className="container mx-auto px-8 py-8">
+        <section className="bg-[#fd9d3e] pt-16 pb-32">
+            <div className="container mx-auto px-8 flex flex-col md:flex-row gap-4 justify-center">
+                <div className=" flex flex-col  md:w-1/2 justify-center text-center">
+                    <img className='h-24' src={order1} />
+                    <h1 className="font-extrablod text-gray-900 py-2 uppercase text-2xl">ORDER YOUR FOOD</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-</p>
+                </div>
 
+                <div className=" flex flex-col  md:w-1/2 justify-center text-center">
+                    <img className='h-24' src={order2} />
+                    <h1 className="font-extrablod text-gray-900 py-2 uppercase text-2xl">DELIVERY OR PICK UP</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-</p>
+                </div>
+
+                <div className=" flex flex-col  md:w-1/2 justify-center text-center">
+                    <img className='h-24' src={order3} />    
+                    <h1 className="font-extrablod text-gray-900 py-2 uppercase text-2xl">DELICIOUS RECEIPE</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius-</p>
+                </div>
             </div>
         </section>
     )
